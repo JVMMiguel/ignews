910352eeaa00857a8301356f5f73832a6996b3ac
JVMMiguel <joaovittorjv@live.com>
2022-02-22 23:07:29 -0300

    First commit since starting classes for the third chapter of rocketseat's ignite trail

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,43 @@
+import { signIn, useSession } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
+import styles from './styles.module.scss'
+
+interface SubscribeButtonProps {
+  priceId: string,
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session} = useSession()
+  async function handleSubscribe() {
+    // Verifica se o usuário está logado antes de ir para a tela de 'checkout'
+    // Caso não esteja, redireciona para o Login e para a aplicação
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs()
+      await stripe.redirectToCheckout({ sessionId })
+
+    } catch (err) {
+
+      alert(err.message)
+
+    }
+  }
+
+  return(
+    <button
+      type='button'
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  )
+}
